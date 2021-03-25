ef4d6084f6005ff23dd541fd3fe0e6bb86cb0dba
Andrea-Mattia <andrea.mattia94@gmail.com>
2021-03-25 17:35:25 +0100

    add snack 2 solution

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@
 // Esegui questo programma in due versioni, con il for e con il while
 
 // dichiaro le variabili che mi serviranno
-var somma = 0;
-var size = 5;
+// var somma = 0;
+// var size = 5;
 
 // Metodo 1: for
 
@@ -23,34 +23,53 @@ var size = 5;
 // Metodo 2: while
 
 // inizializzo il counter a 1
-var counter = 1;
+// var counter = 1;
 
-while (counter <= size) {
-   var userNum = parseInt( prompt('Inserisci il numero ' + counter + ' di ' + size) );
+// while (counter <= size) {
+//    var userNum = parseInt( prompt('Inserisci il numero ' + counter + ' di ' + size) );
 
-   // validazione dato (se non è un numero me lo fa inserire di nuovo all'infinito finchè non lo è)
-   while (isNaN(userNum)) {
-       var userNum = parseInt( prompt('Dato inserito non valido. Inserisci il numero ' + counter + ' di ' + size))
-   }
+//    // validazione dato (se non è un numero me lo fa inserire di nuovo all'infinito finchè non lo è)
+//    while (isNaN(userNum)) {
+//        var userNum = parseInt( prompt('Dato inserito non valido. Inserisci il numero ' + counter + ' di ' + size))
+//    }
 
-   // aggiunta dell' userNum a somma
-   somma += userNum;
+//    // aggiunta dell' userNum a somma
+//    somma += userNum;
 
-   // incremento del counter
-   counter++;
-}
+//    // incremento del counter
+//    counter++;
+// }
 
-// stampa del totale
-console.log('Il totale è:', somma);
+// // stampa del totale
+// console.log('Il totale è:', somma);
 
 
 
 // Snack 2
 // Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo
 
+dichiaro la variabile che mi servirà
+var number = parseInt(prompt('Inserisci un numero'));
+
+// validazione dato (se non è un numero me lo fa inserire di nuovo all'infinito finchè non lo è)
+while (isNaN(number)) {
+   var number = parseInt(prompt('Valore errato. Inserisci un numero'));
+}
+
+// controllo (tramite il modulo) se il numero inserito è pari o dispari e, di conseguenza, scelgo cosa stampare
+if (number % 2 === 0) {
+   console.log('Il tuo numero è pari:', number);
+} else {
+   // uso ++number per incrementare di uno il numero PRIMA che sia letto, in modo da stampare il numero successivo a quello inserito 
+   console.log('Il tuo numero è dispari, stampo il successivo:', ++number);
+}
+
+
 
 // Snack 3
 // Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
+
+
 
 
 // Snack 4
